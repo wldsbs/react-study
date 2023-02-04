44da859332f40b7069417dba26da5c7a0478363e
jiyun <jiyun_99@naver.com>
2023-02-04 14:57:49 +0900

    #6.2 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-// import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // console.log("i run all the time");
-  // useEffect : 무조건 한번만 실행된다(state가 바뀌어도)
-  // deps로 어떤 걸 관찰하느냐에 따라 다름
+function Hello() {
+  //   function byeFn() {
+  //     console.log("bye :(");
+  //   }
+  //   function hiFn() {
+  //     console.log("created :)");
+  //     return byeFn;
+  //   }
+  //   useEffect(hiFn, []);
   useEffect(() => {
-    console.log("i run only once");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-  useEffect(() => {
-    console.log("i run when 'counter' changeds");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when 'keyword' changeds");
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
-      {/* <Button text={"Continue"} /> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
